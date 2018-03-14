a2705ee3252ce7acc88a5c8cf6c3b1b5c8a9982d
pigunther <pigunther16@gmail.com>
2018-03-14 22:03:32 +0300

    rewritten formulas for m\d\1\k

--- scripts/plot_on_one.py
+++ scripts/plot_on_one.py
@@ -6,6 +6,10 @@ import numpy as np
 import argparse
 from scipy.special import factorial
 
+
+
+
+
 def E_func(k, ro):
     sum2 = 1
     for j in range(0, k+1):
@@ -18,6 +22,13 @@ def service_time_for_d(lam, mu, n):
     ws = 1/lam/(1-E_func(n, lam/mu))*(np.sum([E_func(i, lam/mu) for i in range (1, n+1)])) + n/mu*(1-1/(1-E_func(n, lam/mu)))
     return ws
 
+
+
+def b_n(n, ro) :
+    sum = 0
+    for i in range (int(n)+1):
+        sum += (-1 * (n-i) * ro)**i/factorial(i) * np.exp((n-i)*ro)
+    return sum
 
 def pi_func(n, ro):
     n = int(n)
@@ -32,10 +43,7 @@ def pi_func(n, ro):
 
     sum1 += np.exp(n*ro)
     sum1 *= (1-ro)
-    #print("pi:", sum1)
     return sum1
-
-
 
 def refused_prob(k, ro):
     k = int(k)
@@ -48,7 +56,6 @@ def refused_prob(k, ro):
         sum2 = 1
         for j in range(0, k+1):
             sum2 -= ((1-ro)*((-ro*(k-j))**j)*np.exp(ro*(k-j))/factorial(j))
-        #print(sum2)
     return (1-ro)*sum2/(1-ro*sum2)
 
 
@@ -69,42 +76,57 @@ file_name = "service_time_M_"+ model+"_1_"+str(int(k))+service+".png"
 print(file_name + " will be created")
 
 
-
-df = pd.read_table('result.dat', sep=' ', index_col = None, header = None)
-
+df=[0,0,0]
+df[0] = pd.read_table('result_1dr-2.dat', sep=' ', index_col = None, header = None)
+df[1] = pd.read_table('result_3dr-2.dat', sep=' ', index_col = None, header = None)
+df[2] = pd.read_table('result_5dr-2.dat', sep=' ', index_col = None, header = None)
+# print(df[1][3])
+# print(df[0][3])
 
 r = np.arange(0.001, 0.96, 0.01)
 ed = np.ones(86)
-
 plt.figure(figsize=(8,5))
 if (model == 'm') :
-    #yplot = (1-r)*r/(1-r**(k+2))*(r**k+(1-r**k*(k*(1-r)+1))/(1-r)**2)+1
-    #ref = r**(k+1)*(1-r)/(1-r**(k+2))
-    yplot = 1/(1-r)
-    ref = r/(1+r)
+    yplot = (1-r)*r/(1-r**(k+2))*(r**k+(1-r**k*(k*(1-r)+1))/(1-r)**2)+1
+    ref = r**(k+1)*(ed-r)/(ed-r**(k+2))
 
 else :
-    yplot = [service_time_for_d(x, 1, k)+1 for x in r]
-
-    ref = [refused_prob(k, x) for x in r]
+    ref = r/(2*(1-r))
 
 
 if (service == "s"):
-    plt.plot(r, yplot, marker = 'None', linestyle = '-', color = 'g', label = 'analytics')
-    plt.errorbar(df[0], df[1], df[2], marker = 'x', linestyle = 'None', color = 'k', label = 'simulation')
+
+    i = 0
+    color = ['g', 'r', 'b']
+    for k in [1, 3, 5] :
+        plt.plot(r, [service_time_for_d(x, 1, k)+1 for x in r], marker = 'None', linestyle = '-', color = color[i], label = 'analytics k={}'.format(k))
+        plt.errorbar(df[i][0], df[i][1], df[i][2], marker = 'x', linestyle = 'None', color = color[i], label = 'simulation k={}'.format(k))
+        i += 1
+
     plt.ylabel('Average service time')
 
 elif (service == "r"): #refused
-    plt.plot(r, ref, marker = 'None', linestyle = '-', color = 'g', label = 'analytics')
-    plt.errorbar(df[0], df[3], df[4], marker = 'x', linestyle = 'None', color = 'k', label = 'simulation')
-    plt.ylabel('Part of refused packets')
+    i = 0
+    color = ['g', 'r', 'b']
+    for k in [1, 3, 5] :
+        plt.plot(r, [refused_prob(k, x) for x in r], marker = 'None', linestyle = '-', color = color[i], label = 'analytics k={}'.format(k))
+        plt.errorbar(df[i][0], df[i][3], df[i][4], marker = 'x', linestyle = 'None', color = color[i], label = 'simulation k={}'.format(k))
+        plt.ylabel('Part of refused packets')
+        i += 1
+# else:
+#     plt.plot(r, wait, marker = 'None', linestyle = '-', color = 'g', label = 'analytics')
+#     plt.errorbar(df[0], df[5], df[6], marker = 'x', linestyle = 'None', color = 'k', label = 'simulation')
+#     plt.ylabel('Average waiting time')
+
 
 
 
 plt.xlabel(r'$\lambda / \mu$')
 
-plt.title('Model m/{}/1/{}  '.format(model, k))
+#plt.ylabel('Average waiting time')
+plt.title('Model m/{}/1/1-3-5  '.format(model))
 plt.grid()
+# plt.yscale('log')
 plt.legend(loc='best')
 plt.savefig(file_name)
 
